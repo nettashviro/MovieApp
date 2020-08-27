b6cb7870f2d839a2880916c7fae0ba5bdb2d008a
barlotem <barlotem@gmail.com>
2020-08-27 21:01:05 +0300

    Translate models and views to hebrew, add delete button in details pages instead of maintenance the "delete" page, minor form changes. Delete accounts unnecessary views

--- MovieApp/Models/Soundtrack.cs
+++ MovieApp/Models/Soundtrack.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,13 +9,18 @@ namespace MovieApp.Models
     public class Soundtrack
     {
         public int Id { get; set; }
+
+        [Display(Name = "שם")]
         public string Name { get; set; }
 
         // TODO: parse int to display minutes
+        [Display(Name = "משך בדקות")]
         public int Duration { get; set; }
 
+        [Display(Name = "כותב")]
         public string Writer { get; set; }
 
+        [Display(Name = "מבצע")]
         public string Performer { get; set; }
     }
 }
